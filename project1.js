234bb93462979cb0794034be746684b5b195ae6b
Derek <dwklatt@gmail.com>
2015-03-11 13:44:15 -0400

    combined

--- project1.js
+++ project1.js
@@ -1,7 +1,21 @@
 var gl;
 var points;
-var x = 0;
-var y = 0;
+var transX = 0.0;
+var transY = 0.0;
+var canvas;
+var pointsArray = [];
+var program;
+var bufferId;
+var vPosition;
+var cBuffer;
+var vColor;
+var vertexColors = [
+vec4( 0.0, 0.0, 0.0, 1.0 ), // black
+vec4( 1.0, 0.0, 0.0, 1.0 ), // red
+vec4( 1.0, 1.0, 0.0, 1.0 ), // yellow
+vec4( 0.0, 0.0, 1.0, 1.0 ) // blue
+];
+var on = [0,0,0,0];
 var time = 1;
 var score = 0;
 var timer_interval;
@@ -21,84 +35,117 @@ window.onload = function init(){
   score_board.style.left = window_w/2 + 50 + "px"
 
   //webgl stuff
-  var canvas = document.getElementById( "gl-canvas" );
+  canvas = document.getElementById( "gl-canvas" );
   canvas.width = window_w/2;
   canvas.height = window_h - 40;
 
   gl = WebGLUtils.setupWebGL( canvas );
   if ( !gl ) { alert( "WebGL isn't available" ); }
+  
+  pointsArray = [
+		vec2(-0.1, -0.1),
+		vec2(-0.1, 0.1),
+		vec2(0.1, 0.1),
+		vec2(0.1, -0.1)];
+	pointsArray2 = [
+		vec2(-0.4, -0.4),
+		vec2(-0.4, -0.2),
+		vec2(-0.2, -0.2),
+		vec2(-0.2, -0.4)];
+	document.onkeydown = keypressed;
+	document.onkeyup = kup;
+  
   //
   // Configure WebGL
   //
   gl.viewport( 0, 0, canvas.width, canvas.height );
   gl.clearColor( 0.0, 1.0, 0.0, 1.0 );
-  //document.onkeydown = handleKeyDown;
-  //document.onkeyup = handleKeyUp;
+
   // Load shaders and initialize attribute buffers
   var program = initShaders( gl, "vertex-shader", "fragment-shader" );
   gl.useProgram( program );
-  window.onkeydown = function( event ) {
-    switch( event.keyCode ) {
-      //up
-      case 38:
-        if(0.8 > y-0.1){
-          y = y+0.1;
-        }
-        break;
-      //down
-      case 40:
-        if(-0.8 < y-0.1){
-          y = y-0.1;
-        }
-        break;
-      //right
-      case 39:
-        if(0.79 > x-0.1){
-          x = x+0.1;
-        }
-        break;
-      //left
-      case 37:
-        if(-0.9 < x-0.1){
-          x = x-0.1;
-        }
-        break;
-    }
-    var shape = new Float32Array([
-      x, y,
-      x-0.1, y-0.15,
-      x+0.1, y-0.15,
-      x, y-0.2,
-      x+0.1, y-0.05
-    ]);
-
-    var shape2 = new Float32Array([
-      x, y,
-      x-0.1, y-0.15,
-      x+0.1, y-0.15,
-      x, y-0.2,
-      x+0.1, y-0.05
-    ]);
-
-    // Load the data into the GPU
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, shape, gl.STATIC_DRAW );
-    // Associate our shader variables with our data buffer
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
-    // Load the data into the GPU
-    // Associate our shader variables with our data buffer
-    render();
-    //not sure if this should go here or in render?
-    if (hasLost()) { newGame(); }
+  
+  // Load the data into the GPU
+  var bufferId = gl.createBuffer();
+  gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+  gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW );
+  // Associate our shader variables with our data buffer
+  var vPosition = gl.getAttribLocation( program, "vPosition" );
+  gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+  gl.enableVertexAttribArray( vPosition );
+  // Load the data into the GPU
+  // Associate our shader variables with our data buffer
+  
+  cBuffer = gl.createBuffer();
+	gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+	gl.bufferData( gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW );
+	vColor = gl.getAttribLocation( program, "vColor" );
+	gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+	gl.enableVertexAttribArray( vColor );
+	render();
+	//render every 50 ms
+	setInterval(toRender, 50);
+  if (hasLost()) { newGame(); }
   };
 };
 
+function toRender(){
+	if(on[0] == 1){
+		transX -= 0.03;
+	}
+	if(on[1] == 1){
+		transX += 0.03;
+	}
+	if(on[2] == 1){
+		transY += 0.03;
+	}
+	if(on[3] == 1){
+		transY -= 0.03;
+	}
+	if(transX > 0.9)
+		transX = 0.9;
+	if(transX < -0.9)
+		transX = -0.9;
+	if(transY > 0.9)
+		transY = 0.9;
+	if(transY < -0.9)
+		transY = -0.9;
+	vertices = [
+		vec2( -0.1 + transX, -0.1 + transY),
+		vec2( -0.1 + transX, 0.1 + transY),
+		vec2( 0.1 + transX, 0.1 + transY),
+		vec2( 0.1 + transX, -0.1 + transY)];
+	bufferId = gl.createBuffer();
+	gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+	gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+	vPosition = gl.getAttribLocation( program, 'vPosition' );
+	gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+	gl.enableVertexAttribArray( vPosition );
+	render();
+}
+function keypressed(event) {
+	if(event.keyCode == '65')
+		on[0] = 1;
+	else if(event.keyCode == '68')
+		on[1] = 1;
+	else if(event.keyCode == '87')
+		on[2] = 1;
+	else if(event.keyCode == '83')
+		on[3] = 1;
+};
+function kup(event) {
+	if(event.keyCode == '65')
+		on[0] = 0;
+	else if(event.keyCode == '68')
+		on[1] = 0;
+	else if(event.keyCode == '87')
+		on[2] = 0;
+	else if(event.keyCode == '83')
+		on[3] = 0;
+}
 function render() {
-  gl.clear( gl.COLOR_BUFFER_BIT );
-  gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
+	gl.clear( gl.COLOR_BUFFER_BIT );
+	gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
 }
 
 //Starts a new game
