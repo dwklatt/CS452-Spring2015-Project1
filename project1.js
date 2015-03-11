d903e3c5e40382680ea09a2f8540897de2b792e6
Sean O'Byrne <obyrnesm@users.noreply.github.com>
2015-03-11 14:56:05 -0400

    Update project1.js

--- project1.js
+++ project1.js
@@ -11,10 +11,17 @@ var bufferId;
 var vPosition;
 var cBuffer;
 var vColor;
+var n = 0;
 
 var vertexColors = [
         vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
         vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
+        vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
+        vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
+		vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
+        vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
+        vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
+		vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
         vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
         vec4( 0.0, 0.0, 1.0, 1.0 )  // blue
     ];
@@ -98,12 +105,37 @@ window.onload = function init()
     gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vColor );
 
-    render();
+    //render();
 	//render every 50 ms
-	setInterval(toRender, 50);
+	setInterval(toRender, 10);
 
+	  // Set vertex coordinates and point sizes
+  // n = initVertexBuffers();
+  // if (n < 0) {
+    // console.log('Failed to set the vertex information');
+    // return;
+  // }
 
+  // Specify the color for clearing <canvas>
+  gl.clearColor(0.0, 1.0, 0.0, 1.0);
+
+  // Clear <canvas>
+  // gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // Draw three points
+  // gl.drawArrays(gl.POINTS, 0, n);
+	render();
 };
+
+
+
+
+
+
+
+
+
+
 
 
 //This will be where we check to see if the score should be updated
@@ -121,8 +153,7 @@ function updateTime() {
   var timer = document.getElementById("timer");
   timer.innerHTML = "<p>Time: " + time + "</p>";
   time++;
-  	if (!has_lost()) { updateScore(); }
-	else { newGame(); }
+
 }
 
 //Starts a new game
@@ -135,18 +166,19 @@ function newGame() {
 }
 
 
+
 function toRender(){
 	if(on[0] == 1){
-		transX -= 0.03;
+		transX -= 0.01;
 	}
 	if(on[1] == 1){
-		transX += 0.03;
+		transX += 0.01;
 	}
 	if(on[2] == 1){
-		transY += 0.03;
+		transY += 0.01;
 	}
 	if(on[3] == 1){
-		transY -= 0.03;
+		transY -= 0.01;
 	}
 	
 	if(transX > 0.9)
@@ -162,17 +194,71 @@ function toRender(){
 	vec2( -0.1 + transX, -0.1 + transY),
 	vec2( -0.1 + transX, 0.1 + transY),
 	vec2( 0.1 + transX, 0.1 + transY),
-	vec2( 0.1 + transX, -0.1 + transY)]; 
+	vec2( 0.1 + transX, -0.1 + transY)];
+	
+	  	if (!has_lost()) { updateScore(); }
+	else { newGame(); }
 	
 	
-	bufferId = gl.createBuffer();
-	gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-	gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+	// bufferId = gl.createBuffer();
+	// gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+	// gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
 	
-	vPosition = gl.getAttribLocation( program, 'vPosition' );
-	gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-	gl.enableVertexAttribArray( vPosition );
+	// vPosition = gl.getAttribLocation( program, 'vPosition' );
+	// gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+	// gl.enableVertexAttribArray( vPosition );
 
+	
+	
+	var verticesSizes = new Float32Array([
+    // Coordinate and size of points
+     transX,  transY,  10.0,  // the 1st point
+    -0.5, -0.5,  20.0,  // the 2nd point
+     0.5, -0.5,  30.0,   // the 3rd point
+	 0.8, 0.0, 40.0,	// the fourth
+	 1.0, 1.0, 50  	// ...
+  ]);
+  //var n2 = 5; // The number of vertices
+	n = 5;
+  // Create a buffer object
+  var vertexSizeBuffer = gl.createBuffer();  
+  if (!vertexSizeBuffer) {
+    console.log('Failed to create the buffer object');
+    //return -1;
+  }
+
+  // Bind the buffer object to target
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexSizeBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, verticesSizes, gl.STATIC_DRAW);
+
+  var FSIZE = verticesSizes.BYTES_PER_ELEMENT;
+  //Get the storage location of a_Position, assign and enable buffer
+  var vPosition = gl.getAttribLocation(program, 'vPosition');
+  if (vPosition < 0) {
+    console.log('Failed to get the storage location of vPosition');
+    //return -1;
+  }
+  gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, FSIZE * 3, 0);
+  gl.enableVertexAttribArray(vPosition);  // Enable the assignment of the buffer object
+
+  // Get the storage location of a_PointSize
+  var a_PointSize = gl.getAttribLocation(program, 'a_PointSize');
+  if(a_PointSize < 0) {
+    console.log('Failed to get the storage location of a_PointSize');
+    //return -1;
+  }
+  gl.vertexAttribPointer(a_PointSize, 1, gl.FLOAT, false, FSIZE * 3, FSIZE * 2);
+  gl.enableVertexAttribArray(a_PointSize);  // Enable buffer allocation
+
+  // Unbind the buffer object
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+  //return n2;
+	
+	
+	
+	
+	
 
 	render();
 }
@@ -199,6 +285,11 @@ function kup(event) {
 }
 
 function render() {
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
+    // gl.clear( gl.COLOR_BUFFER_BIT );
+    // gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
+	  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // Draw three points
+  gl.drawArrays(gl.POINTS, 0, n);
 }
